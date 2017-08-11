84672c4a2e4cc27f778e3afa1929d131e262f97b
HFrajacomo <henrique.frajacomo@gmail.com>
2017-08-11 06:36:58 -0300

    Update WeaponML.py
    Now shows elapsed time

--- CS_Weapon_Classifier/WeaponML.py
+++ CS_Weapon_Classifier/WeaponML.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+import time as tm
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description='Runs Weapon_Regression.py in a loop', add_help=True)
 parser.add_argument('-t','--tabular', dest='tabular', type=str, help='File containing all info', required=True)
@@ -11,8 +13,12 @@ args = parser.parse_args()
 
 i=0
 
+start_time = datetime.now()
+
 for i in range(0,args.iter):
 	os.system("python Weapon_Regression.py -t " + args.tabular + " -x " + str(args.X_label) + " -y " + str(args.Y_label) + " -l")
 	print("Prediction: " + str(i+1) + " of " + str(args.iter))
 
+print("Learning process finished!")
+print("Elapsed Time: " + str(datetime.now() - start_time))
 os.system("python 3-featurePlot.py -t " + args.tabular)
